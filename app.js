ba5172452e570830cef93fe1cd64f9640c72d590
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2020-07-11 23:05:52 +0200

    sample

--- app.js
+++ app.js
@@ -3,11 +3,48 @@ import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+
+class InsertImage extends Plugin {
+    init() {
+        const editor = this.editor;
+
+        editor.ui.componentFactory.add( 'insertImage', locale => {
+            const view = new ButtonView( locale );
+
+            view.set( {
+                label: 'Insert image',
+                icon: imageIcon,
+                tooltip: true
+            } );
+
+            // Callback executed once the image is clicked.
+            view.on( 'execute', () => {
+                
+                const imageUrl = prompt( 'Image URL' );
+
+                editor.model.change( writer => {
+                    const imageElement = writer.createElement( 'image', {
+                        src: imageUrl
+                    } );
+
+                    // Insert the image in the current selection location.
+                    editor.model.insertContent( imageElement, editor.model.document.selection );
+                } );
+            } );
+
+            return view;
+        } );
+    }
+}
 
 ClassicEditor
     .create( document.querySelector( '#editor' ), {
-        plugins: [ Essentials, Paragraph, Bold, Italic ],
-        toolbar: [ 'bold', 'italic' ]
+        plugins: [ Essentials, Paragraph, Bold, Italic, Image, InsertImage ],
+        toolbar: [ 'bold', 'italic', 'insertImage' ]
     } )
     .then( editor => {
         console.log( 'Editor was initialized', editor );
@@ -15,3 +52,6 @@ ClassicEditor
     .catch( error => {
         console.error( error.stack );
     } );
+
+    
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Variables Plugin
+Plugin to add variables to a CKeditor instance document
+
+## Reference
+
+https://ckeditor.com/docs/ckeditor5/latest/framework/guides/quick-start.html
+https://ckeditor.com/docs/ckeditor5/latest/framework/guides/creating-simple-plugin.html
