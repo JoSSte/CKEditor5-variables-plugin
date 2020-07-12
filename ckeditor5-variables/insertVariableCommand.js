445b4653b4676a5b1c9f95fab1009f818d2a74c6
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2020-07-12 20:21:52 +0200

    Command inserts text

--- ckeditor5-variables/insertVariableCommand.js
+++ ckeditor5-variables/insertVariableCommand.js
@@ -2,28 +2,22 @@ import Command from '@ckeditor/ckeditor5-core/src/command';
 
 export default class InsertVariableCommand extends Command {
 
-    execute() {
+    execute(event) {
         this.editor.model.change(writer => {
-            this.editor.model.insertContent(createVariable(writer));
+            console.log('commandEvent', event);
+            //this.editor.model.insertContent(createVariable(writer, event.param, this.editor.model.document.selection.getFirstPosition()));
+            writer.insertText( event.param, {}, this.editor.model.document.selection.getFirstPosition() );
         });
     }
 
     refresh() {
-        console.log('InsertVariableCommand#refresh() got called');
-        const model = this.editor.model;
-        const selection = model.document.selection;
-        const allowedIn = model.schema.findAllowedParent(selection.getFirstPosition(), 'p');
-
+        //console.log('InsertVariableCommand#refresh() got called');
+        
+        //const model = this.editor.model;
+        //const selection = model.document.selection;
+        //const allowedIn = model.schema.findAllowedParent(selection.getFirstPosition(), 'p');
         //this.isEnabled = allowedIn !== null;
-        this.isEnabled = true;
+
+        this.isEnabled = true;//TODO make sure that this is true only if the cursor position is set
     }
-}
-
-function createVariable(writer) {
-    const variable = writer.createElement('variable');
-    // There must be at least one paragraph for the description to be editable.
-    // See https://github.com/ckeditor/ckeditor5/issues/1464.
-    writer.appendElement('paragraph', variable);
-
-    return variable;
 }
